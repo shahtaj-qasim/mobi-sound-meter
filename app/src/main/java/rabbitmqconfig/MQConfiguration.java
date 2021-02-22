27a14ad1aa6b4f2f797ad0853ba93806120aa305
Shahtaj Qasim <shahtaj.qasim7@gmail.com>
2021-02-22 20:28:31 +0100

    initial commit

---
+++ app/src/main/java/rabbitmqconfig/MQConfiguration.java
@@ -0,0 +1,8 @@
+package rabbitmqconfig;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class MQConfiguration {
+}
